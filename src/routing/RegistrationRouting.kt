4d85ddb47eec44137f0ff70f3655c6c8d533e2b7
liaddd <liaddd@gmail.com>
2021-05-14 18:48:08 +0300

    irrelevant function deleted

--- src/routing/RegistrationRouting.kt
+++ src/routing/RegistrationRouting.kt
@@ -5,15 +5,6 @@ import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
-
-fun Routing.register() {
-    post("/login") {
-        val x = call.request.queryParameters["id"]
-        println("its $x")
-        call.respondText("Hi $x", ContentType.Text.Plain)
-    }
-}
-
 fun Routing.login() {
     get("/login") {
         val x = call.request.queryParameters["id"]
